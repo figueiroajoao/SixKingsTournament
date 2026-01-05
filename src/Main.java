8d3c2d0f05619ca31842a7d5bc7fc3d80a44c037
Joao Figueiroa <joaofigueiroa.dev@gmail.com>
2026-01-05 18:13:26 -0300

    feat: create player class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
 void main() {
-
+    Player p1 = new Player("Carlos Alcaraz", "Spain", 22, 1.83f, 74, 0, 0);
+    p1.playerStats();
+    Player p2 = new Player("Jannik Sinner", "Italy", 24, 1.91f, 77, 0, 0);
+    p2.playerStats();
+    Player p3 = new Player("Alexander Zverev", "Germany", 28, 1.98f, 90, 0, 0);
+    p3.playerStats();
+    Player p4 = new Player("Novak Djokovic", "Serbia", 38, 1.88f, 77, 0, 0);
+    p4.playerStats();
+    Player p5 = new Player("Stefanos Tsitsipas", "Greece", 27, 1.93f, 90, 0, 0);
+    p5.playerStats();
 }
--- src/Player.java
+++ src/Player.java
@@ -9,15 +9,108 @@ public class Player {
     private int losses;
 
     public Player(String name, String country, int age, float height, float weight, int wins, int losses) {
-        this.name = name;
-        this.country = country;
-        this.age = age;
+        this.setName(name);
+        this.setCountry(country);
+        this.setAge(age);
         this.height = height;
-        this.weight = weight;
+        this.setWeight(weight);
         this.wins = wins;
         this.losses = losses;
+
     }
 
+    public void playerStats() {
+        System.out.println(getName() +  " is a " + getCategory() + " from " + getCountry() + "." +
+                            " He is " + getAge() + " years old. His record is: " + getWins() +
+                            " wins and " + getLosses() + " losses");
+    }
+    public void winMatch() {
+        this.setWins(this.getWins() + 1);
+    }
+    public void loseMatch() {
+        this.setLosses(this.getLosses() + 1);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Player must have a name");
+        }
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        if (country == null || country.trim().isEmpty()) {
+            throw new IllegalArgumentException("Country cannot be empty");
+        }
+        this.country = country;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age <= 0) {
+            throw new IllegalArgumentException("Age must be positive");
+        }
+        this.age = age;
+        this.setCategory();
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        if (weight < 20.0 || weight > 100.0) {
+            throw new IllegalArgumentException("Invalid weight");
+        }
+        this.weight = weight;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory() {
+        if (this.age <= 18) {
+            this.category = "Junior Player";
+        } else {
+            this.category = "Pro Player";
+        }
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
+    }
 }
 
 
