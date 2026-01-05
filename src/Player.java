e0270b09a9906873addc6bafc5c8e6aabf982d33
Joao Figueiroa <joaofigueiroa.dev@gmail.com>
2026-01-05 16:53:33 -0300

    feat: create player class

---
+++ src/Player.java
@@ -0,0 +1,24 @@
+public class Player {
+    private String name;
+    private String country;
+    private int age;
+    private float height;
+    private float weight;
+    private String category;
+    private int wins;
+    private int losses;
+
+    public Player(String name, String country, int age, float height, float weight, int wins, int losses) {
+        this.name = name;
+        this.country = country;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.wins = wins;
+        this.losses = losses;
+    }
+
+}
+
+
+
