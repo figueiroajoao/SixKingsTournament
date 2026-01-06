addbdb2d3d4c22f832c1341d2768b094e2b5ac2b
Joao Figueiroa <joaofigueiroa.dev@gmail.com>
2026-01-05 22:06:53 -0300

    feat: matchup

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 void main() {
-    Player player1 = new Player("Carlos Alcaraz", "Spain", 22, 1.83f, 74, 0, 0);
-    player1.playerStats();
-    Player player2 = new Player("Jannik Sinner", "Italy", 24, 1.91f, 77, 0, 0);
-    player2.playerStats();
-    Player player3 = new Player("Alexander Zverev", "Germany", 28, 1.98f, 90, 0, 0);
-    player3.playerStats();
-    Player player4 = new Player("Novak Djokovic", "Serbia", 38, 1.88f, 77, 0, 0);
-    player4.playerStats();
-    Player player5 = new Player("Stefanos Tsitsipas", "Greece", 27, 1.93f, 90, 0, 0);
-    player5.playerStats();
+
+    Player p[] = new Player[6];
+
+    p[0] = new Player("Carlos Alcaraz", "Spain", 22, 1.83f, 74, 0, 0);
+    p[1] = new Player("Jannik Sinner", "Italy", 24, 1.91f, 77, 0, 0);
+    p[2] = new Player("Alexander Zverev", "Germany", 28, 1.98f, 90, 0, 0);
+    p[3] = new Player("Novak Djokovic", "Serbia", 38, 1.88f, 77, 0, 0);
+    p[4] = new Player("Taylor Fritz", "USA", 28, 1.96f, 86, 0, 0);
+    p[5] = new Player("Stefanos Tsitsipas", "Greece", 27, 1.93f, 90, 0, 0);
+
+    Match firstRound = new Match();
+    /* firstRound.matchup(p[0], p[1]);
 }
--- src/Match.java
+++ src/Match.java
@@ -1,39 +1,59 @@
+import java.util.Objects;
+import java.util.Random;
+
 public class Match {
-    private Player p1;
-    private Player p2;
+    private Player player1;
+    private Player player2;
 
-    public Match(Player p1, Player p2) {
-        this.setP1(p1);
-        this.setP2(p2);
-        int sets = 3;
-    }
+    public void matchup(Player player1, Player player2) {
+        setP1(player1);
+        setP2(player2);
 
-    public void matchup() {
-
+        if (!Objects.equals(player1.getCategory(), player2.getCategory())) {
+            throw new IllegalArgumentException("Players must be in the same category");
+        }
+        play();
     }
     public void play() {
+        System.out.println("Player 1: " + this.player1.getName());
+        System.out.println("VS");
+        System.out.println("Player 2: " + this.player2.getName());
 
+        Random random = new Random();
+        int winner = random.nextInt(2);
+        switch (winner) {
+            case 0:
+                System.out.println(player1.getName() + " Won!");
+                this.player1.winMatch();
+                this.player2.loseMatch();
+                break;
+            case 1:
+                System.out.println(player2.getName() + " Won!");
+                this.player2.winMatch();
+                this.player1.loseMatch();
+                break;
+        }
     }
 
     public Player getP1() {
-        return p1;
+        return player1;
     }
 
     public void setP1(Player p1) {
-        if (p1.equals(getP2())) {
+        if (p1 != null && p1 == getP2()) {
             throw new IllegalArgumentException("The tennis match must have two different players");
         }
-        this.p1 = p1;
+        this.player1 = p1;
     }
 
     public Player getP2() {
-        return p2;
+        return player2;
     }
 
     public void setP2(Player p2) {
-        if (p2.equals(getP1())) {
+        if (p2 != null && p2 == getP1()) {
             throw new IllegalArgumentException("The tennis match must have two different players");
         }
-        this.p2 = p2;
+        this.player2 = p2;
     }
 }
